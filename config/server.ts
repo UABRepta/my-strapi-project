3f282e2ff8b8e4300c4a5660816d10ec101a0f42
UABRepta <maksim.repkin@repta.lt>
2025-01-16 23:03:35 +0200

    Updated server

--- config/server.ts
+++ config/server.ts
@@ -1,6 +1,7 @@
 export default ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
+  url: env("RENDER_EXTERNAL_URL", ""), // <-- Это позволяет Render определить публичный URL
   app: {
     keys: env.array("APP_KEYS"), // Ключи приложения (массив)
   },
